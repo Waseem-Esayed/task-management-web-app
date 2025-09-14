01f56d7c58aa89cfc7f9d12fe3f3ac6ce23b6ce8
Waseem <esayed200811waseem@gmail.com>
2025-09-14 12:48:59 +0200

    fixed / added some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/next"
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
@@ -7,32 +6,44 @@ import About from './pages/About';
 import Auth from './pages/Auth';
 import RootLayout from './components/RootLayout';
 import NotFound from './pages/NotFound';
+import ReactGA from "react-ga4";
+import { useEffect } from 'react';
+
+ReactGA.initialize("G-12157702481");
+
+function AppWrapper({ children }) {
+  useEffect(() => {
+    // Nur beim ersten Laden der App
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
+  return children;
+}
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
         <Route path='/' element={<RootLayout />}>
-          <Route index element={<Home />} />
-          <Route path='home' element={<Home />} />
+          <Route index element={<Home />}></Route>
+          <Route path='home' element={<Home />}></Route>
           <Route element={<ProtectedRoute />}>
             <Route path='dashboard' element={<Dashboard />} />
           </Route>
-          <Route path='about' element={<About />} />
+          <Route path='about' element={<About />}></Route>
           <Route path='*' element={<NotFound />} />
         </Route>
 
-        <Route path='auth' element={<Auth />} />
+        <Route path='auth' element={<Auth />}></Route>
       </>
     )
-  )
+  );
 
   return (
-    <>
+    <AppWrapper>
       <RouterProvider router={router} />
-      <Analytics />
-    </>
-  )
+    </AppWrapper>
+  );
 }
 
-export default App
+export default App;
